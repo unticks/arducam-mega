0e81418627392e28db9324baa3cf9a20d9bcf5ea
Sebastian Lauwers <sl@unticks.com>
2023-03-20 23:28:49 +0100

    Add exposure control function

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,7 @@ enum RegisterAddress {
     Brightness = 0x22,
     Contrast = 0x23,
     Saturation = 0x24,
+    Exposure = 0x25,
     WhiteBalanceMode = 0x26,
     #[cfg(feature = "5mp")]
     AutoFocus = 0x29,
@@ -83,6 +84,20 @@ enum RegisterAddress {
     FifoSize1 = 0x45,
     FifoSize2 = 0x46,
     FifoSize3 = 0x47,
+}
+
+/// Values to set the exposure of the camera
+#[derive(Debug, Clone, Default)]
+#[repr(u8)]
+pub enum ExposureLevel {
+    #[default]
+    Default = 0x00,
+    PlusOne = 0x01,
+    MinusOne = 0x02,
+    PlusTwo = 0x03,
+    MinusTwo = 0x04,
+    PlusThree = 0x05,
+    MinusThree = 0x06,
 }
 
 /// Values to set the saturation of the camera
@@ -585,6 +600,15 @@ where
         level: SaturationLevel,
     ) -> Result<(), Error<SPI::Error, Delay::Error>> {
         self.write_reg(RegisterAddress::Saturation, level as u8)?;
+        self.wait_idle()
+    }
+
+    /// Sets the camera's exposure
+    pub fn set_exposure(
+        &mut self,
+        level: ExposureLevel,
+    ) -> Result<(), Error<SPI::Error, Delay::Error>> {
+        self.write_reg(RegisterAddress::Exposure, level as u8)?;
         self.wait_idle()
     }
 }
@@ -1175,6 +1199,23 @@ mod tests {
         let mut spi = spi::Mock::new(&expectations);
         let mut c = ArducamMega::new(&mut spi, delay::MockNoop::new());
         c.set_saturation(SaturationLevel::PlusTwo).unwrap();
+        spi.done();
+    }
+
+    #[test]
+    fn set_exposure_writes_a_reg() {
+        let expectations = [
+            Transaction::transaction_start(),
+            Transaction::write_vec(vec![0xa5, 0x03]),
+            Transaction::transaction_end(),
+            // wait_idle
+            Transaction::transaction_start(),
+            Transaction::transfer(vec![0x44], vec![0x00, 0x00, 0x02]),
+            Transaction::transaction_end(),
+        ];
+        let mut spi = spi::Mock::new(&expectations);
+        let mut c = ArducamMega::new(&mut spi, delay::MockNoop::new());
+        c.set_exposure(ExposureLevel::PlusTwo).unwrap();
         spi.done();
     }
 }
